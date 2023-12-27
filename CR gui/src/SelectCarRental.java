a2024abeb0b8eb64a79a2583c2a9df99bd4db2ae
Aleksandar <aleksandarcq@gmail.com>
2023-12-27 16:15:06 +0100

    more

--- CR gui/src/SelectCarRental.java
+++ CR gui/src/SelectCarRental.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SelectCarRental extends JFrame {
     SelectCarRental(){
         ImageIcon logo = new ImageIcon("../images/logo.png");
         JLabel title = new JLabel();
-
+        JButton backButton = new JButton();
 
         this.setVisible(true);
         this.setLayout(null);
@@ -14,5 +16,15 @@ public class SelectCarRental extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setIconImage(logo.getImage());
         this.getContentPane().setBackground(new Color(203, 202, 205));
+
+        backButton.setBounds(200, 50, 50, 50);
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                StartFrame startFrame = new StartFrame();
+            }
+        });
+        this.add(backButton);
     }
 }
